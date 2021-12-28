6a83cc92c7b949f44285cb812ea1642e645943d1
hakonamatata <jan@sviland.org>
2021-12-28 01:40:59 +0100

    solved new exercise

---
+++ log-levels/LogLevels.cs
@@ -0,0 +1,14 @@
+using System;
+
+static class LogLine
+{
+    public static string Message(string logLine) => logLine.Split(':')[1].Trim();
+
+    public static string LogLevel(string logLine) => logLine.Split(':')[0]
+        .Replace("[", "")
+        .Replace("]", "")
+        .ToLower()
+        .Trim();
+
+    public static string Reformat(string logLine) => $"{Message(logLine)} ({LogLevel(logLine)})";
+}
